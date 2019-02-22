719a7d3a5088ac90c1124630101a353d5a16fa86
Nash Brancic <nashfft@gmail.com>
2019-02-22 02:13:13 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,33 +1,33 @@
-# def third_challenge
-#   epic_tragedy = {
-#   :montague => {
-#       :patriarch => {
-#         :name => "Lord Montague"
-#         :age => "53"
-#         },
-#       :matriarch => {
-#         :name => "Lady Montague"
-#         :age => "54"
-#         },
-#       :hero => {
-#         :name => "Rome"
-#         :age => "15"
-#         },
-#       :hero_friends => []
-#   }, 
-#   :capulet => {
-#       :patriarch => {
-#         #your key/value pairs here
-#         },
-#       :matriarch => {
-#         #your key/value pairs here
-#         },
-#       :heroine => {
-#         #your key/value pairs here
-#         },
-#       :heroine_friends => []
-#   }
-#   }
+def third_challenge
+  epic_tragedy = {
+  :montague => {
+      :patriarch => {
+        :name => "Lord Montague"
+        :age => "53"
+        },
+      :matriarch => {
+        :name => "Lady Montague"
+        :age => "54"
+        },
+      :hero => {
+        :name => "Rome"
+        :age => "15"
+        },
+      :hero_friends => []
+  }, 
+  :capulet => {
+      :patriarch => {
+        #your key/value pairs here
+        },
+      :matriarch => {
+        #your key/value pairs here
+        },
+      :heroine => {
+        #your key/value pairs here
+        },
+      :heroine_friends => []
+  }
+  }
 
   
-# end
+end
