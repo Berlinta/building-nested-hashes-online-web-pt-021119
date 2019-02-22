ef8317ad5b0c63a75fc6d141d1cb59e566ea5139
Nash Brancic <nashfft@gmail.com>
2019-02-22 02:40:05 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -24,6 +24,5 @@ def bonus
 
 end
 epic_tragedy[:capulet][:heroine]{status: "dead"}
-  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-  epic_tragedy
+  #Don't touch the following line! The `bonus` method must return our newl
 end
